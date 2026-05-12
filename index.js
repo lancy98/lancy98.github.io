e64b222adf7150aba333d9c5d30db6e374e847c5
Lancy Norrbert Fernandes <lancy98@gmail.com>
2026-05-12 07:56:32 +0530

    Added footer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks");
+const year = document.getElementById("year");
+
+year.textContent = new Date().getFullYear();
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
